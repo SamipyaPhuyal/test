68c8d296aec87bb34213d70feafb9056572658c5
SamipyaPhuyal <samipyaphuyal012@gmail.com>
2026-04-30 22:08:50 +0545

    testdrfapi

---
+++ users/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(write_only=True)
+    password2=serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields=["password","password2","id","username"]
+        extra_kwargs = {
+            'id': {'read_only': True}
+        }
+
+    def create(self,validated_data):
+        user=User.objects.create_user(
+        username=validated_data["username"],
+        password=validated_data["password"]        
+        )
+        return user
